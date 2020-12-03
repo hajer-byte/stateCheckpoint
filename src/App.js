2ad764a71c15f21eda7f047fb413724f94b7c23e
HAJER <hajer.mejri@contentpepper.de>
2020-12-03 23:45:00 +0100

    state checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import React from "react";
+import { MDBBtn } from "mdbreact";
+class App extends React.Component {
+  state = {
+    Person: {
+      fullName: "Hajer Mejri",
+      bio: "Always aiming to be a rainbow at the end of a thunderstorm!",
+      imgSrc: "./me.jpg",
+      profession: "Front-end developer",
+    },
+    show: "false",
+    counter: 0,
+  };
+  handleClick = () => {
+    this.setState({ show: !this.state.show });
+  };
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({ counter: this.state.counter + 1 });
+    }, 1000);
+  }
+  render() {
+    const { counter } = this.state;
+    return (
+      <>
+        <MDBBtn
+          style={{
+            position: "relative",
+            left: "25%",
+            width: "50%",
+            fontFamily: "Sofia sans-serif",
+          }}
+          onClick={this.handleClick}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          person's profile
+        </MDBBtn>
+        <h1
+          style={{
+            fontFamily: "Sofia sans-serif",
+            textAlign: "center",
+            border: "solid black 2px",
+            backgroundColor: "burlywood",
+          }}
+        >
+          {counter}
+        </h1>
+        {this.state.show && (
+          <div
+            style={{
+              backgroundColor: "bisque",
+              textAlign: "center",
+              fontFamily: "Sofia sans-serif",
+            }}
+          >
+            <h1>{this.state.Person.fullName}</h1>
+            <h2>{this.state.Person.profession}</h2>
+            <h3>{this.state.Person.bio}</h3>
+            <img
+              style={{ width: "600px", height: "500px" }}
+              src={this.state.Person.imgSrc}
+              alt="user pic"
+            />
+          </div>
+        )}
+      </>
+    );
+  }
 }
-
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import reactDOM from "react-dom";
+import react from "react";
+import App from "./App";
+import "bootstrap-css-only/css/bootstrap.min.css";
+import "mdbreact/dist/css/mdb.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function AppF() {
+  return (
+    <react.Fragment>
+      <App />
+    </react.Fragment>
+  );
+}
+const rootElement = document.getElementById("root");
+reactDOM.render(<AppF />, rootElement);
